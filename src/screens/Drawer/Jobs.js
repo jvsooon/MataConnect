960e775b6145cb13936cbcb72d7257cda6d9c395
Aleksandra Trifonova <aleksandra.trifonova.707@my.csun.edu>
2021-03-27 05:17:53 +0000

    very in progress api key integration

--- src/screens/Drawer/Jobs.js
+++ src/screens/Drawer/Jobs.js
@@ -7,4 +7,34 @@ export default function Jobs() {
             <Text>Jobs Screen</Text>
         </View>
     );
+
+
+const https = require('https');
+
+// Replace with access token for the r_liteprofile permission
+const accessToken = 'r_emailadress';
+const options = {
+  host: 'api.linkedin.com',
+  path: '/v2/me',
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${accessToken}`,
+    'cache-control': 'no-cache',
+    'X-Restli-Protocol-Version': '2.0.0'
+  }
+};
+
+const profileRequest = https.request(options, function(res) {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    const profileData = JSON.parse(data);
+    console.log(JSON.stringify(profileData, 0, 2));
+  });
+});
+profileRequest.end();
+
 }
